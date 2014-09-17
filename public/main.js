74fe39b0e2b97a86752127bbb1b0cb4bf270a0f8
ChopperLee2011 <lihengpro@gmail.com>
2014-09-17 15:34:46 +0800

    finish upload feature

--- public/main.js
+++ public/main.js
@@ -1,155 +1,59 @@
-'use strict';
-var fileUpload = angular.module('fileUpload', ['angularFileUpload']);
-fileUpload.controller('fileUploadCtl', function($scope, $http, $timeout, $upload) {
-    // $scope.dropSupported= true;
-
-    $scope.onFileSelect = function($files) {
-        $scope.selectedFiles = [];
-        $scope.progress = [];
-        if ($scope.upload && $scope.upload.length > 0) {
-            for (var i = 0; i < $scope.upload.length; i++) {
-                if ($scope.upload[i] != null) {
-                    $scope.upload[i].abort();
-                }
-            }
-        }
-        $scope.upload = [];
-        $scope.uploadResult = [];
-        $scope.selectedFiles = $files;
-        // $scope.selectedFiles.push($files);
-        $scope.dataUrls = [];
-        for (var i = 0; i < $files.length; i++) {
-            var $file = $files[i];
-            if ($scope.fileReaderSupported && $file.type.indexOf('image') > -1) {
-                var fileReader = new FileReader();
-                fileReader.readAsDataURL($files[i]);
-                var loadFile = function(fileReader, index) {
-                    fileReader.onload = function(e) {
-                        $timeout(function() {
-                            $scope.dataUrls[index] = e.target.result;
-                        });
-                    }
-                }(fileReader, i);
-            }
-            $scope.progress[i] = -1;
-            if ($scope.uploadRightAway) {
-                $scope.start(i);
-            }
-        }
-    };
-    $scope.start = function(index) {
-        $scope.progress[index] = 0;
-        $scope.errorMsg = null;
-        if ($scope.howToSend == 1) {
-            $scope.upload[index] = $upload.upload({
-                url: uploadUrl,
-                method: $scope.httpMethod,
-                headers: {
-                    'my-header': 'my-header-value'
-                },
-                data: {
-                    myModel: $scope.myModel,
-                    errorCode: $scope.generateErrorOnServer && $scope.serverErrorCode,
-                    errorMessage: $scope.generateErrorOnServer && $scope.serverErrorMsg
-                },
-                /* formDataAppender: function(fd, key, val) {
-                    if (angular.isArray(val)) {
-                        angular.forEach(val, function(v) {
-                          fd.append(key, v);
-                        });
-                      } else {
-                        fd.append(key, val);
-                      }
-                }, */
-                /* transformRequest: [function(val, h) {
-                    console.log(val, h('my-header')); return val + '-modified';
-                }], */
-                file: $scope.selectedFiles[index],
-                fileFormDataName: 'myFile'
-            });
-            $scope.upload[index].then(function(response) {
-                $timeout(function() {
-                    $scope.uploadResult.push(response.data);
-                });
-            }, function(response) {
-                if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-            }, function(evt) {
-                // Math.min is to fix IE which reports 200% sometimes
-                $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-            });
-            $scope.upload[index].xhr(function(xhr) {
-                //              xhr.upload.addEventListener('abort', function() {console.log('abort complete')}, false);
-            });
-        } else {
-            var fileReader = new FileReader();
-            fileReader.onload = function(e) {
-                $scope.upload[index] = $upload.http({
-                    url: uploadUrl,
-                    headers: {
-                        'Content-Type': $scope.selectedFiles[index].type
-                    },
-                    data: e.target.result
-                }).then(function(response) {
-                    $scope.uploadResult.push(response.data);
-                }, function(response) {
-                    if (response.status > 0) $scope.errorMsg = response.status + ': ' + response.data;
-                }, function(evt) {
-                    // Math.min is to fix IE which reports 200% sometimes
-                    $scope.progress[index] = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-                });
-            }
-            fileReader.readAsArrayBuffer($scope.selectedFiles[index]);
-        }
-    };
-    $scope.dragOverClass = function($event) {
-        var items = $event.dataTransfer.items;
-        var hasFile = false;
-        if (items != null) {
-            for (var i = 0; i < items.length; i++) {
-                if (items[i].kind == 'file') {
-                    hasFile = true;
-                    break;
-                }
-            }
-        } else {
-            hasFile = true;
-        }
-        return hasFile ? "dragover" : "dragover-err";
-    };
-
-
-
-    $scope.formData = {};
-    $http.get('/api/todos')
-        .success(function(data) {
-            $scope.todos = data;
-            console.log(data)
-        })
-        .error(function(data) {
-            console.log('Error: ' + data);
+/**
+ *  Module
+ *
+ * Description
+ */
+angular.module('fileUpload', ['angularFileUpload'])
+    .controller('fileUploadCtl', ['$scope', 'FileUploader',
+        function($scope, FileUploader) {
+          var uploader = $scope.uploader = new FileUploader({
+            url: 'http://localhost:8080'
         });
+            // FILTERS
 
-    $scope.createTodo = function() {
-        $http.post('/api/todos', $scope.formData)
-            .success(function(data) {
-                //$scope.forData = {};
-                $scope.formData.text = null;
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error:' + data);
+            uploader.filters.push({
+                name: 'customFilter',
+                fn: function(item /*{File|FileLikeObject}*/ , options) {
+                    return this.queue.length < 10;
+                }
             });
-    };
 
-    $scope.deleteTodo = function(id) {
-        $http.delete('/api/todos/' + id)
-            .success(function(data) {
-                $scope.todos = data;
-                console.log(data);
-            })
-            .error(function(data) {
-                console.log('Error: ' + data);
-            });
-    };
-});
+            // CALLBACKS
+
+            uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/ , filter, options) {
+                console.info('onWhenAddingFileFailed', item, filter, options);
+            };
+            uploader.onAfterAddingFile = function(fileItem) {
+                console.info('onAfterAddingFile', fileItem);
+            };
+            uploader.onAfterAddingAll = function(addedFileItems) {
+                console.info('onAfterAddingAll', addedFileItems);
+            };
+            uploader.onBeforeUploadItem = function(item) {
+                console.info('onBeforeUploadItem', item);
+            };
+            uploader.onProgressItem = function(fileItem, progress) {
+                console.info('onProgressItem', fileItem, progress);
+            };
+            uploader.onProgressAll = function(progress) {
+                console.info('onProgressAll', progress);
+            };
+            uploader.onSuccessItem = function(fileItem, response, status, headers) {
+                console.info('onSuccessItem', fileItem, response, status, headers);
+            };
+            uploader.onErrorItem = function(fileItem, response, status, headers) {
+                console.info('onErrorItem', fileItem, response, status, headers);
+            };
+            uploader.onCancelItem = function(fileItem, response, status, headers) {
+                console.info('onCancelItem', fileItem, response, status, headers);
+            };
+            uploader.onCompleteItem = function(fileItem, response, status, headers) {
+                console.info('onCompleteItem', fileItem, response, status, headers);
+            };
+            uploader.onCompleteAll = function() {
+                console.info('onCompleteAll');
+            };
+
+            console.info('uploader', uploader);
+        }
+    ]);
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var app = express()
-var logger = require('morgan')
-var bodyParser = require('body-parser')
-var methodOverride = require('method-override')
-
+var express = require('express');
+var app = express();
+var logger = require('morgan');
+var busboy = require('connect-busboy');
+var path = require('path');
+var fs = require('fs-extra');
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/angualr-todo');
@@ -11,60 +11,75 @@ mongoose.connect('mongodb://localhost:27017/angualr-todo');
 app.use(express.static(__dirname + '/public'));
 app.use("/lib", express.static(__dirname + '/public/lib'));
 app.use(logger('dev'));
-app.use(bodyParser());
-app.use(methodOverride('X-HTTP-Method-Override'));
-
-app.listen(8080, function() {
-    console.log('App listening on port 8080')
-});
+app.use(busboy());
 
 // Definition Model
 var Todo = mongoose.model('todo', {
     text: String
 });
 
-app.get('/api/todos', function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err) {
-            res.sed(err);
-        }
-        res.json(todos);
-    });
-});
+// app.get('/api/todos', function(req, res) {
+//     Todo.find(function(err, todos) {
+//         if (err) {
+//             res.sed(err);
+//         }
+//         res.json(todos);
+//     });
+// });
 
-app.post('/api/todos', function(req, res) {
-    Todo.create({
-        text: req.body.text,
-        done: false
-    }, function(err, todo) {
-        if (err) {
-            res.send(err);
-        }
-        Todo.find(function(err, todos) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(todos);
-        });
-    });
-});
+// app.post('/api/todos', function(req, res) {
+//     Todo.create({
+//         text: req.body.text,
+//         done: false
+//     }, function(err, todo) {
+//         if (err) {
+//             res.send(err);
+//         }
+//         Todo.find(function(err, todos) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json(todos);
+//         });
+//     });
+// });
 
-app.delete('/api/todos/:todo', function(req, res) {
-    Todo.remove({
-        _id: req.params.todo
-    }, function(err, todo) {
-        if (err) {
-            res.send(err);
-        }
-        Todo.find(function(err, todos) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(todos);
-        });
-    })
-});
+// app.delete('/api/todos/:todo', function(req, res) {
+//     Todo.remove({
+//         _id: req.params.todo
+//     }, function(err, todo) {
+//         if (err) {
+//             res.send(err);
+//         }
+//         Todo.find(function(err, todos) {
+//             if (err) {
+//                 res.send(err);
+//             }
+//             res.json(todos);
+//         });
+//     })
+// });
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
     res.sendfile('index.html');
+});
+
+
+app.post('/', function(req, res) {
+    var fstream;
+    req.pipe(req.busboy);
+    req.busboy.on('file', function(fieldname, file, filename) {
+        console.log("Uploading: " + filename);
+
+        //Path where image will be uploaded
+        fstream = fs.createWriteStream(__dirname + '/img/' + filename);
+        file.pipe(fstream);
+        fstream.on('close', function() {
+            console.log("Upload Finished of " + filename);
+            res.redirect('back'); //where to go next
+        });
+    });
+});
+app.listen(8080, function() {
+    console.log('App listening on port 8080')
 });
